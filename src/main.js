b31175ec901780baa0bb81d4129526072fea4d43
Muhammad Vickramdani <muhammadvicramdani@gmail.com>
2020-06-22 19:47:11 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/dist/modal";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import $ from "jquery";
+
+import "regenerator-runtime";
+import "./style.css";
+
+AOS.init();
+
+$(document).ready(function() {
+  autoPlayYouTubeModal();
+});
+
+function autoPlayYouTubeModal() {
+  var trigger = $(".trigger");
+  trigger.click(function(e) {
+    e.preventDefault();
+    const theModal = $(this).data("target");
+    const videoSRC = $(this).data("src");
+    const videoSRCauto = videoSRC + "?autoplay=1";
+    $(theModal + " iframe").attr("src", videoSRCauto);
+    $(theModal).on("hidden.bs.modal", function() {
+      $(theModal + " iframe").attr("src", "");
+    });
+  });
+}
